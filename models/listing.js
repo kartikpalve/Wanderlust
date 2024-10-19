439814047d1fbb211c63f512b554e97a601b9390
kartikpalve <palve.kartik101004@gmail.com>
2024-10-19 16:51:59 +0530

    done  till create reviews on 19 oct

--- models/listing.js
+++ models/listing.js
@@ -1,21 +1,32 @@
-const mongoose=require("mongoose");
-const Schema= mongoose.Schema;
+const mongoose = require("mongoose");
+const Review = require("./reviews");
+const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-
-    title:{
-        type:String,
-        require:true,
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://tse3.mm.bing.net/th?id=OIP.B8u7c37kE5ZdM5ZCRm-4hwAAAA&pid=Api&P=0&h=180",
+    set: (v) =>
+      v === ""
+        ? "https://tse3.mm.bing.net/th?id=OIP.B8u7c37kE5ZdM5ZCRm-4hwAAAA&pid=Api&P=0&h=180"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref:"Review",
     },
-    description:String,
-    image:{type:String,
-        default :"https://tse3.mm.bing.net/th?id=OIP.B8u7c37kE5ZdM5ZCRm-4hwAAAA&pid=Api&P=0&h=180",
-        set: (v)=>v==="" ? "https://tse3.mm.bing.net/th?id=OIP.B8u7c37kE5ZdM5ZCRm-4hwAAAA&pid=Api&P=0&h=180" : v,
-    },
-    price:Number,
-    location:String,
-    country:String,
+  ],
 });
 
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
